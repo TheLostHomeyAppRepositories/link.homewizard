233b4c6e8d0b81f461c32f89bd5a3dc9fe27b8a4
smarthomesven <sven@dypodex.com>
2026-01-11 19:37:19 +0100

    Added support for Smartwares type smoke detector

--- drivers/co-detector/driver.js
+++ drivers/co-detector/driver.js
@@ -8,6 +8,10 @@ module.exports = class CODriver extends BaseHomeWizardDriver {
     return "sw_mesh_smoke_detector";
   }
 
+  getDeviceModel() {
+    return "rf868_mesh_co_detector_siterwell"
+  }
+
   async onInit() {
     this.log('CO detector driver has been initialized');
   }
